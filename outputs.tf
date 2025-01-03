fe21a807bff1c0bbd314ddcef48479e503078939
Arjen Huitema <arjenhuitema@microsoft.com>
2025-01-03 21:35:11 +0100

    Update output value for user assigned managed identity in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,5 @@ output "resource_group_name" {
 
 output "user_assigned_managed_identity_name" {
   description = "<the user assigned managed identity name"
-  value       = module.amba-uami.name
+  value       = module.amba-uami.resource_name
 }
