819a97b6e82cfadac3f9a87b90ea05acbc802742
Arjen Huitema <arjenhuitema@microsoft.com>
2025-01-08 13:40:02 +0100

    Update output references in outputs.tf for consistency

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "resource_group_name" {
   description = "The resource group name"
-  value       = module.amba-resourcegroup.name
+  value       = module.resource_group.name
 }
 
 output "user_assigned_managed_identity_name" {
   description = "<the user assigned managed identity name"
-  value       = module.amba-uami.resource_name
+  value       = module.user_assigned_managed_identity.resource_name
 }
