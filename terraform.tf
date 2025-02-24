ea4ea506a74f33f2f64b53139177c655676d7cc3
Arjen Huitema <arjenhuitema@microsoft.com>
2025-02-24 13:59:15 +0100

    Remove unused provider configurations from terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,22 +1,14 @@
 terraform {
   required_version = "~> 1.9"
   required_providers {
-    alz = {
-      source  = "Azure/alz"
-      version = "~> 0.16"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.71"
     }
-    azapi = {
-      source  = "Azure/azapi"
-      version = "~> 2.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.5"
-    }
+    //local = {
+     // source  = "hashicorp/local"
+      //version = "~> 2.5"
+    //}
     modtm = {
       source  = "azure/modtm"
       version = "~> 0.3"
