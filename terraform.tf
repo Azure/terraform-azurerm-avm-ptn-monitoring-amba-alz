769ed31ba95e1ec8f69bdbaf24ef780213e003d3
Arjen Huitema <arjenhuitema@microsoft.com>
2025-02-28 00:26:19 +0100

    Add azapi provider configuration to terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.71"
     }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~> 2.2"
+    }
     modtm = {
       source  = "azure/modtm"
       version = "~> 0.3"
