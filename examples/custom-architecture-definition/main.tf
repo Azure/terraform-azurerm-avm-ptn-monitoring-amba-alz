9512e79a497edc75a84a46ac68ae23b55eefe0b1
Arjen Huitema <arjenhuitema@microsoft.com>
2025-03-24 22:35:55 +0100

    Add resource_group_name and user_assigned_managed_identity_name variables to examples

--- examples/custom-architecture-definition/main.tf
+++ examples/custom-architecture-definition/main.tf
@@ -31,6 +31,17 @@ variable "location" {
   default     = "swedencentral"
 }
 
+variable "resource_group_name" {
+  type        = string
+  default     = "rg-amba-monitoring-001"
+  description = "The resource group where the resources will be deployed."
+}
+
+variable "user_assigned_managed_identity_name" {
+  type        = string
+  default     = "id-amba-prod-001"
+  description = "The name of the user-assigned managed identity."
+}
 variable "action_group_email" {
   description = "Action group email"
   type        = list(string)
@@ -71,10 +82,12 @@ module "amba_policy" {
   location           = var.location
   parent_resource_id = data.azapi_client_config.current.tenant_id
   policy_default_values = {
-    amba_alz_management_subscription_id = jsonencode({ value = var.management_subscription_id != "" ? var.management_subscription_id : data.azapi_client_config.current.subscription_id })
-    amba_alz_resource_group_location    = jsonencode({ value = var.location })
-    amba_alz_resource_group_tags        = jsonencode({ value = var.tags })
-    amba_alz_action_group_email         = jsonencode({ value = var.action_group_email })
-    amba_alz_arm_role_id                = jsonencode({ value = var.action_group_arm_role_id })
+    amba_alz_management_subscription_id          = jsonencode({ value = var.management_subscription_id != "" ? var.management_subscription_id : data.azapi_client_config.current.subscription_id })
+    amba_alz_resource_group_location             = jsonencode({ value = var.location })
+    amba_alz_resource_group_name                 = jsonencode({ value = var.resource_group_name })
+    amba_alz_resource_group_tags                 = jsonencode({ value = var.tags })
+    amba_alz_user_assigned_managed_identity_name = jsonencode({ value = var.user_assigned_managed_identity_name })
+    amba_alz_action_group_email                  = jsonencode({ value = var.action_group_email })
+    amba_alz_arm_role_id                         = jsonencode({ value = var.action_group_arm_role_id })
   }
 }
